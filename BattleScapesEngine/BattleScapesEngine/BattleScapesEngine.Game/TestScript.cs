3d3aa74533ef83db8194b0737d7e58ac2945970b
nug700 <jon.rurka@yahoo.com>
2015-05-12 17:22:48 -0700

    playing with scripts.

---
+++ BattleScapesEngine/BattleScapesEngine/BattleScapesEngine.Game/TestScript.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiliconStudio.Paradox.Engine;
+
+namespace BattleScapesEngine
+{
+    public class TestScript : Script
+    {
+        public override void Start()
+        {
+            base.Start();
+        }
+    }
+}
